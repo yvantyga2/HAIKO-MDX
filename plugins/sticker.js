91652a100cfe1df903aa3121c014892310102adb
Professeur-xd <xdprofesseur@gmail.com>
2025-05-01 02:12:58 -0400

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -6,8 +6,8 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const packname = global.packname || "PROFESSEUR-XD";
-  const author = global.author || "🖤🇵🇰";
+  const packname = global.packname || "HAIKO-MDX";
+  const author = global.author || "🎗️";
 
   const validCommands = ['sticker', 's', 'autosticker'];
 
