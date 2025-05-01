5d120d4beaf73eb1fc33dc0356ffdefc60115ea5
Professeur-xd <xdprofesseur@gmail.com>
2025-05-01 01:40:54 -0400

    Update crick.js

--- plugins/crick.js
+++ plugins/crick.js
@@ -28,7 +28,7 @@ const text = m.body.slice(prefix.length + cmd.length).trim();
       const result = response.data;
 
       let formattedResult = `╭══════════════•∞•══╮\n`;
-      formattedResult += `│⿻   *PROFESSEUR XD 🔥*\n`;
+      formattedResult += `│⿻   *HAIKO-MDX 🔥*\n`;
       formattedResult += `│⿻   *LIVE MATCH INFO* ✨\n`;
       formattedResult += `│⿻\n`;
 
